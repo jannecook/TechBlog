87f1b60ddf8190978ec36d784e3951a07f123de0
jannecook <135638400+jannecook@users.noreply.github.com>
2023-12-10 22:27:44 -0800

    add server.js file

---
+++ server.js
@@ -0,0 +1,6 @@
+// Dependencies
+const express = require('express');
+// Import express-handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
