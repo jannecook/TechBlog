531d7e82a2ba739ba94a9b9c7db88e05b1ed151a
jannecook <135638400+jannecook@users.noreply.github.com>
2023-12-10 22:36:15 -0800

    update to include express and handlebars

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const path = require('path');
+
+// Set up the Express
+const app = express();
+const PORT = process.env.PORT || 3001;
